322e76d3d15fdb5f013533d7fa317c03c44b1b0e
VladHnyp0 <vladhnypprogrammer@gmail.com>
2023-04-18 00:52:05 +0300

    first-commit

---
+++ src/us/lviv/lgs/car/Engine.java
@@ -0,0 +1,11 @@
+package us.lviv.lgs.car;
+
+public class Engine {
+    int cylinders;
+
+    public Engine(int cylinders) {
+        this.cylinders = cylinders;
+    }
+
+
+}
---
+++ src/us/lviv/lgs/car/Wheel.java
@@ -0,0 +1,12 @@
+package us.lviv.lgs.car;
+
+public class Wheel {
+    int wheelDiametr;
+    String wheelMaterial;
+
+    public Wheel(int wheelDiametr, String wheelMaterial) {
+        this.wheelDiametr = wheelDiametr;
+        this.wheelMaterial = wheelMaterial;
+    }
+
+}
